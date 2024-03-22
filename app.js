a66948349c225b34fafbd5568ad44f707eb222d4
Hamzabaloch08 <hb.developer08@gmail.com>
2024-03-23 03:14:40 +0500

    js practice and revice old chapters

--- app.js
+++ app.js
@@ -7,9 +7,13 @@
 var userInput = +prompt("Enter your age");
 
 if (userInput < 18) {
-  document.write("you are under age");
+  document.write("you are under Age for this program");
 } else if (userInput >= 18 && userInput <= 30) {
   document.write("your eligible for this program");
 } else if (userInput > 30){
-    document.write("You are over age for this program");
+    document.write("You are over Age for this program");
+} else if (isNaN(userInput)) {
+    alert("Please Enter Valid Input")
+} else{
+    alert("Invalid input")
 }
