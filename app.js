7a49347df80b8eca12e2c1f8d87fab6ddc3632d7
Hamzabaloch08 <hb.developer08@gmail.com>
2024-03-23 03:41:45 +0500

    practice old chapters

--- app.js
+++ app.js
@@ -1,19 +1,22 @@
-// alert("hello world")
+// // alert("hello world")
 
-// var userInput = prompt("enter any number and alphabate")
+// // var userInput = prompt("enter any number and alphabate")
 
-// document.write(userInput)
+// // document.write(userInput)
 
-var userInput = +prompt("Enter your age");
+// var userInput = +prompt("Enter your age");
 
-if (userInput < 18) {
-  document.write("you are under Age for this program");
-} else if (userInput >= 18 && userInput <= 30) {
-  document.write("your eligible for this program");
-} else if (userInput > 30){
-    document.write("You are over Age for this program");
-} else if (isNaN(userInput)) {
-    alert("Please Enter Valid Input")
-} else{
-    alert("Invalid input")
-}
+// if (userInput < 18) {
+//   document.write("you are under Age for this program");
+// } else if (userInput >= 18 && userInput <= 30) {
+//   document.write("your eligible for this program");
+// } else if (userInput > 30){
+//     document.write("You are over Age for this program");
+// } else if (isNaN(userInput)) {
+//     alert("Please Enter Valid Input")
+// } else{
+//     alert("Invalid input")
+// }
+
+var arr = [1,2,3,4,5,6,7]
+
